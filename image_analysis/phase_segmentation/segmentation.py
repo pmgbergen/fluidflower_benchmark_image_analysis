f349295c13848ac7f174c67d6f310b8351e9b271
Jakub Both <jakub.both@uib.no>
2023-02-14 00:15:21 +0100

    Add phase segmentation.

---
+++ image_analysis/phase_segmentation/segmentation.py
@@ -0,0 +1,27 @@
+"""
+Phase segmentation for all runs of the FluidFlower International Benchmark.
+"""
+from src.io import read_paths_from_user_data
+from src.largerigco2analysis import LargeRigCO2Analysis
+
+# Fix config file for all runs.
+config = "./config.json"
+
+# Loop through all runs
+for run in ["c1", "c2", "c3", "c4", "c5"]:
+
+    # Read user-defined paths to images etc.
+    images, baseline, _, results = read_paths_from_user_data(
+        "../data.json",
+        run
+    )
+
+    # Define FluidFlower based on a full set of basline images
+    co2_analysis = LargeRigCO2Analysis(
+        baseline=baseline,  # paths to baseline images
+        config=config,  # path to config file
+        results=results,  # path to results directory
+    )
+
+    # Perform standardized CO2 segmentation on all images
+    co2_analysis.batch_segmentation(images, plot_contours=False)
